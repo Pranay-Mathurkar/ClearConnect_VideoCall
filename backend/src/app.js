c76049759c9c93dfff8ec21b6e75f5aa56cb4430
Pranay Mathurkar <pranaymnnit123@gmail.com>
2025-07-27 10:42:17 +0530

    Ready for Deployment

---
+++ backend/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "../src/routes/user.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://aura:test1234@cluster0.alskgd8.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst:`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
+
+
+
+start();
